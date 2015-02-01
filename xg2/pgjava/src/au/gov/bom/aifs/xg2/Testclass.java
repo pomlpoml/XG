fdd31a82556df6768a3a9266bfabb9bffaf78c4e
pmemmel <p.memmel@bom.gov.au>
2015-02-02 10:35:19 +1100

    initial setup

---
+++ xg2/pgjava/src/au/gov/bom/aifs/xg2/Testclass.java
@@ -0,0 +1,10 @@
+package au.gov.bom.aifs.xg2;
+
+
+public class Testclass {
+
+	public static void main (String[] args)
+	{
+		System.out.println("huhu");
+	}
+}
---
+++ xg3/pgjava/cap/src/au/gov/bom/aifs/xg3/Testclass.java
@@ -0,0 +1,9 @@
+package au.gov.bom.aifs.xg3;
+
+public class Testclass {
+
+	public static void main (String[] args)
+	{
+		System.out.println("huhu");
+	}
+}
